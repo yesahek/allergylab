c216ca43726d092e66054e9e6be5d70e82dc1951
yesahek <yesahek@gmail.com>
2026-08-02 12:09:06 +0300

    adding workflow

--- Part5a/db-loader/dbconnect.py
+++ Part5a/db-loader/dbconnect.py
@@ -1,12 +1,16 @@
 import mysql.connector
+import os
+db_name = os.environ['DB_NAME' ]
+db_user = os.environ['DB_USER' ]
+db_pass = os.environ['DB_PASS' ]
 
 def connection():
     # Edited out actual values
     conn = mysql.connector.connect( host='dbhost',
                             port=3306,
-                            database='allergen_info',
-                            user='pythonapp',
-                            password='secretPass123!')
+                            database=db_name,
+                            user=db_user,
+                            password=db_pass)
     c = conn.cursor()
 
     return c, conn
