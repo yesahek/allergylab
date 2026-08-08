eded55dd6882a9fbc8600f1d50046c4752424eb8
yesahek <yesahek@gmail.com>
2026-08-08 13:03:31 +0300

    Deploying Into  kubernetes

--- Part9/app/dbconnect.py
+++ Part9/app/dbconnect.py
@@ -4,10 +4,11 @@ import os
 db_name = os.environ['DB_NAME']
 db_user = os.environ['DB_USER']
 db_pass = os.environ['DB_PASS']
+db_host = os.environ['DB_HOST']
 
 def connection():
     # Edited out actual values
-    conn = mysql.connector.connect( host='dbhost',
+    conn = mysql.connector.connect( host='db_host',
                             port=3306,
                             database=db_name,
                             user=db_user,
